ccc90e06bee698e38c0503390c223a6144b88794
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-09-13 16:16:36 +0900

    Refactor code and finalize design

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 let pwdField = document.querySelector("#password");
-let pwdFieldConf = document.querySelector("#password-confirm");
-let submitBtn = document.querySelector("#submit-form");
+let pwdFieldConf = document.querySelector("#confirmpassword");
+let submitBtn = document.querySelector("#form-submit");
 
 let checkPasswords = function (e) {
-    let pwd1 = pwdField.value;
-    let pwd2 = pwdFieldConf.value;
+  let pwd1 = pwdField.value;
+  let pwd2 = pwdFieldConf.value;
 
-    if (pwd1 === pwd2) {
-        console.log("Success!");
-        pwdField.classList.remove("error");
-        pwdFieldConf.classList.remove("error");
+  if (pwd1 === pwd2) {
+    console.log("Success!");
+    pwdField.classList.remove("error");
+    pwdFieldConf.classList.remove("error");
 
-        let warnText = document.querySelector(".password-item p");
-        warnText.parentNode.removeChild(warnText);
+    let warnText = document.querySelector(".password p");
+    warnText.parentNode.removeChild(warnText);
+  } else {
+    e.preventDefault();
+    if (pwdField.classList.contains("error")) {
+      return;
     } else {
-        e.preventDefault();
-        pwdField.classList.add("error");
-        pwdFieldConf.classList.add("error");
-        
-        let warnText = document.createElement("p");
-        warnText.innerText = "* passwords do not match";
-        warnText.classList.add("errorSubtext");
-        let pwdItem = document.querySelector(".password-item");
-        pwdItem.appendChild(warnText);
+      pwdField.classList.add("error");
+      pwdFieldConf.classList.add("error");
+
+      let warnText = document.createElement("p");
+      warnText.innerText = "* passwords do not match";
+      warnText.classList.add("errorSubtext");
+      let pwdItem = document.querySelector(".password");
+      console.log(pwdItem);
+      pwdItem.parentElement.appendChild(warnText);
     }
+  }
 };
 
 submitBtn.addEventListener("click", checkPasswords);
